938ea56a3c31e7a6520b791326d1297d6582fc58
ailyfdez <ailyfdez89@gmail.com>
2021-08-20 17:28:52 -0300

    add error actions

--- src/app/products/state/product.effects.ts
+++ src/app/products/state/product.effects.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { ProductService } from '../product.service';
+
+/* NgRx */
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as ProductActions from './product.actions';
 
 @Injectable()
 export class ProductEffects {
-  /**
-   *
-   */
-  constructor(private actions$: Actions, private productService: ProductService) {
 
-  }
+  constructor(private actions$: Actions, private productService: ProductService) { }
 
   loadProducts$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(ProductActions.loadProducts),
-      mergeMap(() => this.productService.getProducts().pipe(
-        map(products => ProductActions.loadProductsSuccess({ products }))
-      ))
-    );
+    return this.actions$
+      .pipe(
+        ofType(ProductActions.loadProducts),
+        mergeMap(() => this.productService.getProducts()
+          .pipe(
+            map(products => ProductActions.loadProductsSuccess({ products })),
+            catchError(error => of(ProductActions.loadProductsFailure({ error })))
+          )
+        )
+      );
   });
-
 }
